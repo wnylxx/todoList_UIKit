02f5282527063696a0b07b175dabe74a1cf713a6
WonyeolHEO <hyy96hyy@naver.com>
2024-05-24 11:28:46 +0900

    TodoListTableViewCell 컴포넌트 위치 설정

--- TodoList/TodoListView/TodoListTableViewCell.swift
+++ TodoList/TodoListView/TodoListTableViewCell.swift
@@ -37,9 +37,35 @@ class TodoListTableViewCell: UITableViewCell {
         addSubview(checkButton)
         addSubview(dateLabel)
         addSubview(titleLabel)
+        
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = self.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            checkButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            checkButton.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor),
+            checkButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            checkButton.widthAnchor.constraint(equalToConstant: 60),
+            
+            titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: checkButton.trailingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
+            
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            dateLabel.leadingAnchor.constraint(equalTo: checkButton.trailingAnchor, constant: 10),
+            dateLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -8)
+        ])
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implementedrh")
     }
+    
+    
+    
+    
 }
