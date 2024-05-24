5e71714e0bd1647684dedfe87eed8e000f05bbd0
WonyeolHEO <hyy96hyy@naver.com>
2024-05-24 18:19:52 +0900

    Shared 파일 생성

---
+++ TodoList/SharedData.swift
@@ -0,0 +1,66 @@
+//
+//  SharedData.swift
+//  TodoList
+//
+//  Created by wonyoul heo on 5/24/24.
+//
+
+import UIKit
+
+class SharedData {
+    static let shared = SharedData()
+    private var todoEntries: [TodoEntry]
+    
+    private init() {
+        todoEntries = []
+    }
+    
+    func numberOfTodoEntries() -> Int {
+        todoEntries.count
+    }
+    
+    func getTodoEntry(index: Int) -> TodoEntry {
+        todoEntries[index]
+    }
+    
+    func getAllTodoEntries() -> [TodoEntry] {
+        let readOnlyTodoEntries = todoEntries
+        return readOnlyTodoEntries
+    }
+    
+    func addTodoEntry(newTodoEntry: TodoEntry) {
+        todoEntries.append(newTodoEntry)
+    }
+    
+    func removeTodoEntry(index: Int){
+        todoEntries.remove(at: index)
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadTodoEntries() {
+        let fileURL = getDocumentDirectory().appendingPathComponent("todoEntries.json")
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let todoEntriesData = try JSONDecoder().decode([TodoEntry].self, from: data)
+        } catch {
+            print("Failed to read Json data: \(error.localizedDescription)")
+        }
+    }
+    
+    func saveTodoEntries() {
+        let pathDirectory = getDocumentDirectory()
+        try? FileManager.default.createDirectory(at: pathDirectory, withIntermediateDirectories: true)
+        let fileURL = pathDirectory.appendingPathComponent("todoEntries.json")
+        let json = try? JSONEncoder().encode(todoEntries)
+        do {
+            try json!.write(to: fileURL)
+        } catch {
+            print("Faild to write Json data: \(error.localizedDescription)")
+        }
+    }
+    
+}
--- TodoList/TodoEntry.swift
+++ TodoList/TodoEntry.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TodoEntry {
+class TodoEntry: NSObject ,Codable {
     let date: Date
     let entryTitle: String?
     let entryBody: String?
@@ -19,5 +19,25 @@ class TodoEntry {
         self.entryTitle = title
         self.entryBody = body
         self.isCompleted = isCompleted
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case date, entryTitle, entryBody, isCompleted
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        date = try container.decode(Date.self, forKey: .date)
+        entryTitle = try container.decode(String.self, forKey: .entryTitle)
+        entryBody = try container.decode(String.self, forKey: .entryBody)
+        isCompleted = try container.decode(Bool.self, forKey: .isCompleted)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(date, forKey: .date)
+        try container.encode(entryTitle, forKey: .entryTitle)
+        try container.encode(entryBody, forKey: .entryBody)
+        try container.encode(isCompleted, forKey: .isCompleted)
     }
 }
